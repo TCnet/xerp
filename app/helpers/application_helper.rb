574944d49a4d07edd8817cba782aa6b95604d7ec
Hxmusic <hxmusic@foxmail.com>
2018-01-05 08:50:18 +0800

    modify title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def full_title(page_title = '')
     
   # 根据所在的页面返回完整的标题 def full_title(page_title = '')
-    base_title = "BBS only for nbtcnet friends"
+    base_title = "XERP for Amazon"
     if page_title.empty?
       base_title
     else
