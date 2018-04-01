e73285af73e49cc755eb2d55fb32b5b59b880b6d
Hxmusic <hxmusic@foxmail.com>
2018-04-01 17:55:32 +0800

    some update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,7 +11,7 @@ set :server_name, "xerp.top www.xerp.top"
 # at filepaths
 set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
-server '104.237.159.138', user: 'deploy', roles: %w{web app db}, primary: true
+server '212.111.43.142', user: 'deploy', roles: %w{web app db}, primary: true
 
 set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
 
